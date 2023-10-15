f05a4a33dc0641de0381ff7c47014d305d87627e
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2023-10-15 14:30:02 +0200

    useState

--- src/components/Feedback/FeedbackList.jsx
+++ src/components/Feedback/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { Section } from './Section/Section';
 import { Buttons } from './Buttons/Buttons';
@@ -7,59 +7,61 @@ import { Notification } from './Notification/Notification';
 
 import css from './FeedbackList.module.css';
 
-export class FeedbackList extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const FeedbackList = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onClickButton = option => {
-    this.setState(prevState => {
-      return {
-        [option]: prevState[option] + 1,
-      };
-    });
+  const onClickButton = option => {
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
   //  Сума загальних відгуків
-  countTotalFeedback = () =>
-    this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => good + neutral + bad;
 
   // Відсоток позитивних відгуків
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback > 0
-      ? Math.round((this.state.good / totalFeedback) * 100)
+  const countPositiveFeedbackPercentage = () => {
+    return countTotalFeedback() > 0
+      ? Math.round((good / countTotalFeedback()) * 100)
       : 0;
   };
 
-  render() {
-    const keys = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
+  return (
+    <div>
+      <h1 className={css.title}>Please leave feedback</h1>
 
-    return (
-      <div>
-        <h1 className={css.title}>Please leave feedback</h1>
+      <Section title="Feedback Options">
+        <Buttons
+          keys={Object.keys({ good, neutral, bad })}
+          onClickButton={onClickButton}
+        />
+      </Section>
 
-        <Section title="Feedback Options">
-          <Buttons keys={keys} onClickButton={this.onClickButton} />
-        </Section>
-
-        <Section title="Statistics">
-          {totalFeedback > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={totalFeedback}
-              percentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            percentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
